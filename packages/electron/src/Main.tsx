3003b8824c3bd933a39b5a40f07119a3e61d5a2a
pfrantz <pfrantz@blackdogsoftware.com.au>
2023-02-14 17:26:14 +1100

    just a few chages to electorn

--- packages/electron/src/Main.tsx
+++ packages/electron/src/Main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from "@my-app/app";
+import {Animated, View} from "react-native";
+import Text = Animated.Text;
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <View><Text>Hello</Text></View>
     </React.StrictMode>
 );
